744420496939fe69bd2dff098549c0d3a3e72304
Luis Santos <luishv551@gmail.com>
2023-05-17 00:46:15 -0300

    Estudando PL/SQL

---
+++ FASE 3/Consultas_PLSQL_FASE3.sql
@@ -0,0 +1,54 @@
+SET SERVEROUTPUT ON
+DECLARE
+  v_nome    VARCHAR2(30);
+  v_cargo   VARCHAR2(30);
+BEGIN
+  SELECT ename, job
+  INTO v_nome, v_cargo
+  FROM emp
+  WHERE empno = 7934;
+DBMS_OUTPUT.PUT_LINE(v_nome);
+DBMS_OUTPUT.PUT_LINE(v_cargo);
+END;
+/
+
+SET SERVEOUTPUT ON
+DECLARE    
+  v_soma_sal   NUMBER; 
+  v_deptno	 NUMBER NOT NULL := 10;           
+BEGIN
+  SELECT	SUM(sal)  
+  INTO		v_soma_sal
+  FROM		emp
+  WHERE	deptno = v_deptno;
+DBMS_OUTPUT.PUT_LINE('A soma dos salários do departamento ' || v_deptno || ' é ' || v_soma_sal);
+END;
+/
+
+DECLARE
+v_empno NUMBER := 11;
+v_ename VARCHAR2(13) := 'SANDRA';
+v_job VARCHAR2(13) := 'GERENTE';
+v_deptno NUMBER := 10;
+BEGIN
+   INSERT INTO emp(empno, ename, job, deptno)
+          VALUES (v_empno, v_ename, v_job, v_deptno);
+END;
+/
+
+DECLARE
+  v_sal_increase   NUMBER := 2000;
+BEGIN
+  UPDATE	emp
+  SET		sal = sal + v_sal_increase
+  WHERE	job = 'ANALYST';
+END;
+/
+
+DECLARE
+  v_deptno   NUMBER := 10;               
+BEGIN							
+  DELETE FROM   emp
+  WHERE         deptno = v_deptno;
+END;
+/
